60a6a0b2190759fe435e29687a4ce6232d0e18d5
Mohd Hafizuddin M Marzuki <hafizuddin_83@yahoo.com>
2025-04-04 18:13:37 +0800

    Improve content delivery

--- routes/web.php
+++ routes/web.php
@@ -3,39 +3,23 @@
 use Apih\JsLang\JsLang;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get(config('jslang.public_lang_dir') . '/{locale}/{type}{any?}.js', function (string $locale, string $type) {
+Route::get(config('jslang.public_lang_dir') . '/{locale}/{type}{any?}.js', static function (string $locale, string $type) {
     $isLocal = app()->isLocal();
     $jsLang = app(JsLang::class);
 
-    if ($isLocal) {
-        $content = $jsLang->getContents($locale, $type);
-    } else {
-        $cacheDuration = config('jslang.cache_duration');
-        $cacheKey = "jslang-{$locale}-{$type}";
-        $content = cache($cacheKey);
+    $cacheDuration = $isLocal ? 3 : config('jslang.cache_duration');
+    $cacheKey = "jslang-{$locale}-{$type}";
+    $content = cache()->remember($cacheKey, $cacheDuration, static fn () => $jsLang->getContents($locale, $type, !$isLocal));
 
-        if (empty($content)) {
-            $content = $jsLang->getContents($locale, $type, true);
-            cache([$cacheKey => $content], $cacheDuration);
-        }
-    }
-
-    return response($content)->header('Content-Type', 'application/javascript');
+    return response($content)->withHeaders([
+        'Cache-Control' => "public, max-age={$cacheDuration}, s-maxage={$cacheDuration}, immutable",
+        'Content-Type' => 'application/javascript; charset=utf-8',
+        'Etag' => '"' . hash(version_compare(PHP_VERSION, '8.1', '>=') ? 'xxh128' : 'md5', $content) . '"',
+    ]);
 })
-->where([
-    'locale' => implode('|', config('jslang.locales')),
-    'type' => 'short|long|all',
-    'any' => '.*',
-])
-->name(config('jslang.route_name'));
+    ->where([
+        'locale' => implode('|', config('jslang.locales')),
+        'type' => 'short|long|all',
+        'any' => '.*',
+    ])
+    ->name(config('jslang.route_name'));
